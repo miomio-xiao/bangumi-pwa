8f66071f7e24936e793161aa5046324c6ea3cdf4
miomio-xiao <miomio.xiao@gmail.com>
2018-03-12 00:26:18 +0800

    workboxOptions

--- vue.config.js
+++ vue.config.js
@@ -15,5 +15,36 @@ module.exports = {
       }
     }
   },
-  lintOnSave: false
+  lintOnSave: false,
+  pwa: {
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      // swSrc: 'dist/service-worker.js',
+      swDest: './service-worker.js',
+      importWorkboxFrom: 'local',
+      cacheId: 'bangumi',
+      globDirectory: 'dist',
+      globPatterns: ['**/*.{html,js,css,eot,svg,ttf,woff}'],
+      globIgnores: ['service-worker.js', '**/*.map'],
+      include: [/\.jpg$/, /\.png$/],
+      runtimeCaching: [
+        {
+          urlPattern: /api/,
+          handler: 'networkFirst',
+          options: {
+            networkTimeoutSeconds: 10,
+            cacheName: 'api-cache',
+            expiration: {
+              maxEntries: 20,
+              maxAgeSeconds: 60 * 60 * 24 * 7
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+              // headers: { 'x-test': 'true' }
+            }
+          }
+        }
+      ]
+    }
+  }
 };
