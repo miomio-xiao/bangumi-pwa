091c63e3e3a52f45938bb5d80ed74edb3847d3cb
miomio-xiao <miomio.xiao@gmail.com>
2018-03-14 14:29:55 +0800

    cache

--- vue.config.js
+++ vue.config.js
@@ -20,13 +20,13 @@ module.exports = {
     workboxPluginMode: 'GenerateSW',
     workboxOptions: {
       importWorkboxFrom: 'local',
-      swDest: './service-worker.js',
       cacheId: 'bangumi',
       globDirectory: 'dist',
       globPatterns: ['*/*.{html,js,css,eot,svg,ttf,woff}'],
       globIgnores: ['**/*.map'],
       clientsClaim: true,
       skipWaiting: true,
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
       runtimeCaching: [
         {
           urlPattern: /api/,
@@ -45,12 +45,10 @@ module.exports = {
         },
         {
           urlPattern: new RegExp('http://lain.bgm.tv/pic/(.*)'),
-          handler: 'networkFirst',
+          handler: 'cacheFirst',
           options: {
-            networkTimeoutSeconds: 10,
             cacheName: 'images-cache',
             expiration: {
-              maxEntries: 20,
               maxAgeSeconds: 7 * 24 * 60 * 60
             },
             cacheableResponse: { statuses: [0, 200, 503] }
