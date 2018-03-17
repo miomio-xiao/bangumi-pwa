2950e1e4e1d0e2ceb5a0d615f2874b2e8cb0970f
miomio-xiao <miomio.xiao@gmail.com>
2018-03-17 23:43:58 +0800

    remove service-worker config

--- vue.config.js
+++ vue.config.js
@@ -16,45 +16,45 @@ module.exports = {
     }
   },
   lintOnSave: false,
-  pwa: {
-    workboxPluginMode: 'GenerateSW',
-    workboxOptions: {
-      importWorkboxFrom: 'local',
-      cacheId: 'bangumi',
-      globDirectory: 'dist',
-      globPatterns: ['*/*.{html,js,css,eot,svg,ttf,woff}'],
-      globIgnores: ['**/*.map'],
-      clientsClaim: true,
-      skipWaiting: true,
-      dontCacheBustUrlsMatching: /\.\w{8}\./,
-      runtimeCaching: [
-        {
-          urlPattern: /api/,
-          handler: 'networkFirst',
-          options: {
-            networkTimeoutSeconds: 10,
-            cacheName: 'api-cache',
-            expiration: {
-              maxEntries: 20,
-              maxAgeSeconds: 60 * 60 * 24 * 7
-            },
-            cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
-        },
-        {
-          urlPattern: new RegExp('http://lain.bgm.tv/pic/(.*)'),
-          handler: 'cacheFirst',
-          options: {
-            cacheName: 'images-cache',
-            expiration: {
-              maxAgeSeconds: 7 * 24 * 60 * 60
-            },
-            cacheableResponse: { statuses: [0, 200, 503] }
-          }
-        }
-      ]
-    }
-  }
+  // pwa: {
+  //   workboxPluginMode: 'GenerateSW',
+  //   workboxOptions: {
+  //     importWorkboxFrom: 'local',
+  //     cacheId: 'bangumi',
+  //     globDirectory: 'dist',
+  //     globPatterns: ['*/*.{html,js,css,eot,svg,ttf,woff}'],
+  //     globIgnores: ['**/*.map'],
+  //     clientsClaim: true,
+  //     skipWaiting: true,
+  //     dontCacheBustUrlsMatching: /\.\w{8}\./,
+  //     runtimeCaching: [
+  //       {
+  //         urlPattern: /api/,
+  //         handler: 'networkFirst',
+  //         options: {
+  //           networkTimeoutSeconds: 10,
+  //           cacheName: 'api-cache',
+  //           expiration: {
+  //             maxEntries: 20,
+  //             maxAgeSeconds: 60 * 60 * 24 * 7
+  //           },
+  //           cacheableResponse: {
+  //             statuses: [0, 200]
+  //           }
+  //         }
+  //       },
+  //       {
+  //         urlPattern: new RegExp('http://lain.bgm.tv/pic/(.*)'),
+  //         handler: 'cacheFirst',
+  //         options: {
+  //           cacheName: 'images-cache',
+  //           expiration: {
+  //             maxAgeSeconds: 7 * 24 * 60 * 60
+  //           },
+  //           cacheableResponse: { statuses: [0, 200, 503] }
+  //         }
+  //       }
+  //     ]
+  //   }
+  // }
 };
